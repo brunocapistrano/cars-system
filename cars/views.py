f27210d9b0ab93ab8b9d831f5db642f5903ed682
Bruno Capistrano <brunocapistrano303@gmail.com>
2026-06-30 12:54:03 -0300

    keeping

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from cars.models import Car
+from cars.forms import CarForm
 
 
 def cars_view(request):
@@ -15,4 +16,14 @@ def cars_view(request):
         request, 
         'cars.html', 
         {'cars': cars}
+    )
+
+def new_car_view(request):
+
+    new_car_form = CarForm()
+
+    return render(
+        request,
+        'new_car.html',
+        {'new_car_form': new_car_form}
     )
