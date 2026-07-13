5acd13c82c735760c42f95e55d8c4bd84245689a
brunocapistrano <brunokreuch303@gmail.com>
2026-07-12 21:12:59 -0300

    signals final

--- cars/signals.py
+++ cars/signals.py
@@ -1,10 +1,10 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 from django.db.models import Sum
 
 from cars.models import Car, CarInventory
-    
 
+   
 def car_inventory_update():
     cars_count = Car.objects.all().count()
     cars_value = Car.objects.aggregate(
@@ -15,6 +15,12 @@ def car_inventory_update():
         cars_count=cars_count,
         cars_value=cars_value
     )
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        instance.bio = f"This is a {instance.model} from {instance.factory_year} with plate {instance.plate} and value of {instance.value}." # Possbilidade de ser gerado com IA.
 
 
 @receiver(post_save, sender=Car)
